2171220cb003b5c0a8289ecca60346ab87e1db28
lijianwei <1467920854@qq.com>
2024-02-28 14:54:08 +0800

    后端初始化

--- code-generator-basic/src/main/java/com/ljw/generator/MainGenerator.java
+++ code-generator-basic/src/main/java/com/ljw/generator/MainGenerator.java
@@ -15,17 +15,31 @@ import java.io.IOException;
  */
 public class MainGenerator {
     public static void doGenerate(Object model) throws TemplateException, IOException {
-        String projectPath = System.getProperty("user.dir");
-        // 整个项目的根路径
-        File parentFile = new File(projectPath).getParentFile();
-        // 输入路径
-        String inputPath = new File(parentFile, "code-generator-demo-projects/acm-template").getAbsolutePath();
-        String outputPath = projectPath;
-        // 生成静态文件
-        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
-        // 生成动态文件
-        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
-        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/ljw/acm/MainTemplate.java";
-        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+//        String inputRootPath ="code-generator/code-generator-demo-projects/acm-template-pro";
+//        String outputRootPath = "code-generator/acm-template";
+//
+//        String inputPath;
+//        String outputPath;
+//
+//        inputPath = new File(inputRootPath,"src/com/ljw/acm/MainTemplate.java.ftl").getAbsolutePath();
+//        outputPath = new File(outputRootPath,"src/com/ljw/acm/MainTemplate.java").getAbsolutePath();
+//        DynamicGenerator.doGenerate(inputPath, outputPath, model);
+        String inputRootPath = "D:\\code-generator\\code-generator-demo-projects\\acm-template-pro";
+        String outputRootPath = "D:\\code-generator\\code-generator-demo-projects\\acm-template-pro";
+
+        String inputPath;
+        String outputPath;
+
+        inputPath = new File(inputRootPath, "src/com/ljw/acm/MainTemplate.java.ftl").getAbsolutePath();
+        outputPath = new File(outputRootPath, "src/com/ljw/acm/MainTemplate.java").getAbsolutePath();
+        DynamicGenerator.doGenerate(inputPath, outputPath, model);
+
+        inputPath = new File(inputRootPath, ".gitignore").getAbsolutePath();
+        outputPath = new File(outputRootPath, ".gitignore").getAbsolutePath();
+        StaticGenerator.copyFilesByHutool(inputPath, outputPath);
+
+        inputPath = new File(inputRootPath, "README.md").getAbsolutePath();
+        outputPath = new File(outputRootPath, "README.md").getAbsolutePath();
+        StaticGenerator.copyFilesByHutool(inputPath, outputPath);
     }
 }
